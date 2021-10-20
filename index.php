3e0ce516aed8586a99c1a4262af0319b18b3205b
Hayyi123SM <68420790+Hayyi123SM@users.noreply.github.com>
2021-10-20 08:54:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Ng. Satria Utomo W.P.(155610024)</h3>";3>";
+<<<<<<< HEAD
+echo "<h3>Hayyi Shinshan Mahesa(205410057)</h3>"
+=======
+>>>>>>> upstream/master
+?>
